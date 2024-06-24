6d6a62dd5cb62517d8f05691f7fa4d33e56316ee
jvanhyning <45504971+jvanhyn@users.noreply.github.com>
2024-06-24 14:18:12 -0700

    Update helloWorld.cpp
    Changed output format

--- src/helloWorld.cpp
+++ src/helloWorld.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <iomanip> 
+#include <limits> 
 #include <rc/mpu.h>
 
 static rc_mpu_data_t mpu_data;
@@ -9,7 +11,10 @@ int main(void){
     for(int i = 0; i < 10; i++){
         for(i=0;i<3;i++){
             accel_vec[i]=mpu_data.accel[i];
-            std::cout << std::format("{}", std::numbers::accel_vec[i]);
+            cout << fixed 
+                 << setprecision( 
+                        numeric_limits<double>::max_digits10) 
+                 << accel_vec[i] << endl;
             } 
     }
 
