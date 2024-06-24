b3fb16191d443c7652e14e50942adee080d50927
jvanhyning <45504971+jvanhyn@users.noreply.github.com>
2024-06-24 14:57:09 -0700

    Update helloWorld.cpp

--- src/helloWorld.cpp
+++ src/helloWorld.cpp
@@ -2,8 +2,21 @@
 #include <printf.h>
 
 static rc_mpu_data_t mpu_data;
+// static rc_mpu_data_t gyr_data;
+// static rc_mpu_data_t mag_data;
+
+rc_mpu_config_t conf = rc_mpu_default_config();
+
 
 int main(void){
+
+    conf.enable_magnetometer = 1;
+
+    rc_mpu_initialize(mpu_data,conf);
+
+    rc_mpu_read_accel(mpu_data);
+    rc_mpu_read_gyro(mpu_data);
+    rc_mpu_read_mag(mpu_data);
 
     printf(" %5.2f %5.2f %5.2f |", data.accel[0],\
                                    data.accel[1],\
