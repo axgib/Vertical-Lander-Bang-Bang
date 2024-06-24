d25e45e421e69423bd48e078d06dcc0dd0d95bce
jvanhyning <45504971+jvanhyn@users.noreply.github.com>
2024-06-24 14:03:34 -0700

    Update helloWorld.cpp

--- src/helloWorld.cpp
+++ src/helloWorld.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+static rc_mpu_data_t mpu_data;
+double accel_vec[3];
 
 int main(void){
+
     for(int i = 0; i < 10; i++){
-    std::cout << "Hello World!";
+        for(i=0;i<3;i++) accel_vec[i]=mpu_data.accel[i];
+        std::cout << accel_vec;
     }
+    
     return 0;
 }
